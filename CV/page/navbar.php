fba9908847175d94920635dad432e59613aadd4f
NguyenQuy2002 <quy.nguyen2002@hcmut.edu.vn>
2023-05-08 16:28:46 +0700

    make search form

--- CV/page/navbar.php
+++ CV/page/navbar.php
@@ -1,28 +1,31 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary shadow-sm">
-  <div class="container-fluid">
-  <a class="navbar-brand" href="<?=SITE_URL?>">
-      <img src="assets/images/logo.png" alt="" height="30" class="d-inline-block align-text-top">
-      CV Builder Online
-    </a>
-      <ul class="navbar-nav">
-<?php
-if($action->user_id())
-{
-  ?>
-   <li class="nav-item">
-          <a class="nav-link <?=@$myresume?>" aria-current="page" href="<?=$action->helper->url('home')?>"><i class="bi bi-file-earmark-medical-fill"></i> My Resumes</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link <?=@$profile?>" href="#"><i class="bi bi-person-workspace"></i> Profile</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?=$action->helper->url('action/logout')?>"><i class="bi bi-box-arrow-left"></i> Logout</a>
-        </li>
-  <?php
-}
-?>
-       
-      </ul>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="<?= SITE_URL ?>">
+            <img src="assets/images/logo.png" alt="" height="30" class="d-inline-block align-text-top">
+            CV Builder Online
+        </a>
+        <ul class="navbar-nav">
+            <?php
+            if ($action->user_id()) {
+            ?>
+                <li class="nav-item">
+                    <a class="nav-link <?= @$myresume ?>" aria-current="page" href="<?= $action->helper->url('home') ?>"><i class="bi bi-file-earmark-medical-fill"></i> My Resumes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link <?= @$profile ?>" href="#"><i class="bi bi-person-workspace"></i> Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= $action->helper->url('search') ?>"><i class="bi bi-search"></i>
+                        Search CVs</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?= $action->helper->url('action/logout') ?>"><i class="bi bi-box-arrow-left"></i> Logout</a>
+                </li>
+            <?php
+            }
+            ?>
 
-  </div>
+        </ul>
+
+    </div>
 </nav>

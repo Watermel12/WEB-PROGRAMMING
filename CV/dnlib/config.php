9d78d13d27d3f2b85942080f65f210e01168284b
NguyenQuy2002 <quy.nguyen2002@hcmut.edu.vn>
2023-05-09 19:23:33 +0700

    Finish program

--- CV/dnlib/config.php
+++ CV/dnlib/config.php
@@ -4,8 +4,8 @@
 const DB_NAME = 'cv2';
 const DB_HOST = 'localhost';
 const DB_USER = 'root';
-const DB_PASS = '';
+const DB_PASS = 'thang2711';
 
 //URL
-const SITE_URL= 'http://localhost/WEB-PROGRAMMING/CV/';
+const SITE_URL= 'http://localhost/CV/';
 const ASSET_URL=SITE_URL.'assets';
